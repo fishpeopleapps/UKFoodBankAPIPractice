b4e70dc49d1f980b2b30a7841b5b163da73eb977
Kimmy Brewer <127132791+fishpeopleapps@users.noreply.github.com>
2023-10-17 14:26:59 -0600

    Initial Commit

---
+++ UKFoodBankAPIPractice/UKFoodBankAPIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UKFoodBankAPIPracticeApp.swift
+//  UKFoodBankAPIPractice
+//
+//  Created by Kimberly Brewer on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct UKFoodBankAPIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
